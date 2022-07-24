9745877bbf9876a2691ee1d4e8e91e653efbde6d
Losevsa <losevsa@inbox.ru>
2022-07-24 23:00:20 +0300

    Добавьте файлы проекта.

---
+++ SuperDog/SuperDog.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+	virtual void voice() = 0;
+
+};
+
+class Dog : virtual public Animal
+{
+public:
+	void voice()
+	{
+		std::cout << "Meow" << std::endl;
+	}
+};
+
+class Cat : virtual public Animal
+{
+public:
+	void voice()
+	{
+		std::cout << "Bark" << std::endl;
+	}
+};
+
+
+int main()
+{
+	Dog d;
+	d.voice();
+	Animal* cat = new Cat();
+	cat->voice();
+	return 0;
+}
